054658aa5d27ac653075f6eaa0aeaee91705265c
Alex Safonov <alks.saf@gmail.com>
2020-08-06 12:46:52 +0300

    First commit

---
+++ js/lesson6.js
@@ -0,0 +1,21 @@
+"use strict";
+let number = 4.6;
+console.log(-4/0);
+console.log('string'*9);
+
+let person = `5`;
+
+let bool = true;
+
+let und;
+console.log(und);
+const obj = {
+    name: `John`,
+    age: 25,
+    isMarried: false
+};
+
+// console.log(obj.name);
+console.log(obj['name']);
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp', 5, {}, []];
+console.log(arr[2]);

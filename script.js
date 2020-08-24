38ae5f9b16738761e431d3a67b859be751091b32
Alex Safonov <alks.saf@gmail.com>
2020-08-24 20:31:13 +0300

    FUnctions practice

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-"use strict"
-const numberOfFilms = +prompt('Сколько фильмов уже посмотрели?', '');
+"use strict";
+let numberOfFilms;
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов уже посмотрели?', '');
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов уже посмотрели?', '');
+
+    }
+}
+start();
 console.log(numberOfFilms);
 const personalMovieBD = {
     count: numberOfFilms,
@@ -8,11 +16,52 @@ const personalMovieBD = {
     genres:[],
     privat:false
 };
-const a = prompt ("Какой филь смотрели в последнее время?",''),
-      b = +prompt ("Насколько его оцените?",''),
-      c = prompt ("Какой филь смотрели в последнее время?",''),
-      d = +prompt ("Насколько его оцените?",'');
-personalMovieBD.movie[a] = b;
-personalMovieBD.movie[c] = d;
 
-console.log(personalMovieBD);
+function writeYourGenres() {
+    for (let i = 0; i < 3; i++) {
+        let p = i+1;
+        personalMovieBD.genres[i] = prompt('Ваш люимый жанр №' + p +" ?", '');
+    }
+}
+
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt("Какой филь смотрели в последнее время?",''),
+              b = +prompt("Насколько его оцените?",'');
+        if (a != null && b != null && a != ' ' && b !=' ' && a.length < 50) {
+            personalMovieBD.movie[a] = b;
+        } else {
+            i--;
+        }
+    }
+
+}
+
+rememberMyFilms();
+
+
+function detectPersonalLevel() {
+
+    if (personalMovieBD.count < 10) {
+        alert('Просмотрено довольно мало фильмов'); 
+    }
+    else if (personalMovieBD.count >= 10 & personalMovieBD.count < 30 ) {
+        alert('Вы классический зритель'); 
+    }
+    else if (personalMovieBD.count >= 30) {
+        alert('Вы киноман');
+    }
+    else {
+        alert('Произошла ошибка');  
+    }   
+}
+detectPersonalLevel();
+
+function showMyDB () {
+    if (personalMovieBD.privat == false) {
+        console.log(personalMovieBD);
+
+    }
+}
+writeYourGenres();
+showMyDB();
